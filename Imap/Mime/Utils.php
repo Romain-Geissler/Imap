dc4c23133bfa5e9b3d5c57854cd6c05183bdd5b4
Romain Geissler <romain.geissler@gmail.com>
2012-07-06 16:00:06 +0200

    Add a new Utils class.

---
+++ Imap/Mime/Utils.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Imap\Mime;
+
+class Utils{
+	//$include => only mime headers are kept
+	//!$include => all but mime headers are kept
+	public static function filterMimeHeaders($rawHeaders,$include){
+		$rawHeaders=substr($rawHeaders,0,-2);
+		$matches=[];
+
+		if(($count=preg_match_all('/^([\x21-\x39\x3B-\x7E]+):/m',$rawHeaders,$matches,PREG_SET_ORDER|PREG_OFFSET_CAPTURE))===false){
+			throw new MimeException('Unexpected regex error.');
+		}
+
+		$filteredHeaders=[];
+
+		foreach($matches as $i=>$match){
+			if(!$include^(bool)preg_match('/content-/i',$match[1][0])){
+				if($i!=$count-1){
+					$filteredHeader=substr($rawHeaders,$match[0][1],$matches[$i+1][0][1]-$match[0][1]-2);
+				}else{
+					$filteredHeader=substr($rawHeaders,$match[0][1],-2);
+				}
+
+				$filteredHeaders[]=$filteredHeader;
+			}
+		}
+
+		return implode("\r\n",$filteredHeaders)."\r\n\r\n";
+	}
+}
